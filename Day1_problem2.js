e127bd2915a53c3c4b94fff74684b60470298b5e
suman kumar <sumanadi420@gmail.com>
2022-10-28 19:19:19 +0530

    Day1:-variable assignment

---
+++ Day1_problem2.js
@@ -0,0 +1,6 @@
+let name= "Suman Kumar";
+console.log(name);
+father_name="Lal Bahadur Sah";
+console.log(father_name);
+mother_name="Sheela Devi";
+console.log(mother_name);
---
+++ Day1_problem3.js
@@ -0,0 +1,7 @@
+
+
+let name="Suman Kumar";
+let age=24;
+console.log(name,age);
+console.log(typeof name,typeof age);
+
